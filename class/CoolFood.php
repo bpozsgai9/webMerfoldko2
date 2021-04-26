a6d4c2ef026e6d29a0441acf933c3e5b4404566e
Barni <52259958+bpozsgai9@users.noreply.github.com>
2021-04-26 13:22:22 +0200

    notAnImage

--- class/CoolFood.php
+++ class/CoolFood.php
@@ -191,6 +191,18 @@ class CoolFood {
             $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
             $uploadOk = 1;
             $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+            
+            $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);    
+            if($check !== false) {
+                    
+                $uploadOk = 1;
+
+            } else {
+                    
+                echo "Hiba: A fájl nem kép!";
+                $uploadOk = 0;
+            }
         
             if ($uploadOk == 0) {
             
